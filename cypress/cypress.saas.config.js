6a690ff12e6b2c6dbd0384e556adabe9ade53ebb
suranjanabasu <49215513+suranjanabasu@users.noreply.github.com>
2025-07-08 13:20:43 -0500

    Initialize acap-1062

---
+++ cypress/cypress.saas.config.js
@@ -0,0 +1,48 @@
+const { defineConfig } = require("cypress");
+const baseConfig = require("./cypress.base.config");
+
+// A private user used with AEM Assets testing suite.
+const AEM_ASSETS_PRIVATE_USER = JSON.parse(
+  process.env.AEM_ASSETS_PRIVATE_USER ?? "{}"
+);
+
+module.exports = defineConfig({
+  ...baseConfig,
+  env: {
+    ...baseConfig.env,
+    graphqlEndPoint: "https://na1-sandbox.api.commerce.adobe.com/LwndYQs37CvkUQk9WEmNkz/graphql",
+    giftCardA: "00419VQ5C341",
+    productUrlWithOptions:
+      "/products/cypress-configurable-product-latest/CYPRESS456?optionsUIDs=Y29uZmlndXJhYmxlLzkzLzEz",
+    stateShippingId: "TX,57",
+    stateBillingId: "NY,43",
+    productImageName: "/adb150.jpg",
+    productImageNameConfigurable: "/adb124_1.jpg",
+
+    aemAssetsConfig: {
+      commerceConfig: {
+        coreEndpoint: "https://na1-sandbox.api.commerce.adobe.com/QhUjcEq9dMrdCF7h8a4e5g/graphql",
+        endpoint: "https://na1-sandbox.api.commerce.adobe.com/QhUjcEq9dMrdCF7h8a4e5g/graphql",
+      },
+
+      author: {
+        programId: "p150710",
+        environmentId: "e1553109",
+        isStage: false,
+      },
+
+      credentials: {
+        xPublicApiKey: "",
+        magentoEnvironmentId: "",
+      },
+
+      user: {
+        ...AEM_ASSETS_PRIVATE_USER,
+        order: "000000001",
+        returnedOrder: "000000002",
+      },
+
+      prexDraft: "/drafts/decepticons/prex-discovery",
+    },
+  },
+});
